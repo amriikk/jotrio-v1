af71365034fa4edee533343e2360e53853fe65c1
John Trujillo <amriikk@gmail.com>
2023-02-20 02:12:40 -0800

    clean up nav bar + all the icons and update css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './nav.css'
+import { BiHome } from 'react-icons/bi'
+import { BiUser } from 'react-icons/bi'
+import { FiBook } from 'react-icons/fi'
+import { MdWork } from 'react-icons/md'
+import { RiServiceLine } from 'react-icons/ri'
+import { MdMessage } from 'react-icons/md'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="./"><BiHome /></a>
+      <a href="#about"><BiUser /></a>
+      <a href="#experience"><FiBook /></a>
+      <a href="#experience"><RiServiceLine /></a>
+      <a href="#portfolio"><MdWork /></a>
+      <a href="#contact"><MdMessage /></a>
+
+    </nav>
   )
 }
 
