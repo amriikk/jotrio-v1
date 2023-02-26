bd9bfb5172977696f97c70b683a52b2d408d89bb
John Trujillo <amriikk@gmail.com>
2023-02-25 23:57:00 -0800

    update About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,17 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>About
+    <h5>Get To Know</h5>
+    <h2>About Me</h2>
+    <div className="container about__container">
+      <div className="about__me">
+        <div className="about__me-image">
+          <img src={ME} alt="" />
+        </div>
+      </div>
+    </div>    
+    </section>
   )
 }
 
