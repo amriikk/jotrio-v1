6768c529866160759e0bc5d636b4e4ff48e0bc50
John Trujillo <amriikk@gmail.com>
2023-02-27 01:03:30 -0800

    update about me cards

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,9 +7,10 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id='about'>About
+    <section id='about'>
       <h5>Get To Know</h5>
       <h2>About Me</h2>
+
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
@@ -23,6 +24,26 @@ const About = () => {
               <FaAward className='about__icon' />
               <h5>Experience</h5>
               <small>4+ Years</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>Want to know more?</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>Check out my Portfolio</small>
             </article>
           </div>
         </div>
