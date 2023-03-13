68b19809b330f65dc5d5488c9ef8f777e089c970
John Trujillo <amriikk@gmail.com>
2023-03-13 01:55:47 -0700

    start of wk10

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
         <div className="container header__container">
             <h5>Hello I'm</h5>
-            <h1>J. T.</h1>
+            <h1>J.T.</h1>
             <h5 className='text-light'>Software Engineer</h5>
             <CTA />
             <Socials />
