d62fdc2ac25b503ccfb1df9b652c154c8e4369cf
John Trujillo <amriikk@gmail.com>
2023-07-29 23:50:40 -0700

    update Experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>Skills I Have </h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+            </article>
+          </div>
+          <h3>Backend Development</h3>
+          <article className='experience__details'>
+              <BsPatchCheckFill />
+            </article>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
