7def863707ed636ce024c489219c2573fd736517
John Trujillo <amriikk@gmail.com>
2023-09-01 03:57:16 -0700

    update css for about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,40 +18,45 @@ const About = () => {
           </div>
         </div>
 
+        <br /><br /><br />
+
         <div className="about__content">
           <div className="about__cards">
+
             <article className='about__card'>
               <FaAward className='about__icon' />
               <h5>Experience</h5>
               <small>5+ Years</small>
             </article>
-
             <article className='about__card'>
               <FiUsers className='about__icon' />
               <h5>Clients</h5>
               <small>Want to know more?</small>
             </article>
-        
             <article className='about__card'>
               <VscFolderLibrary className='about__icon' />
               <h5>Projects</h5>
               <small>View Portfolio</small>
             </article>
+
           </div>
         </div>
-      </div>        
-        <br />
-        <div className='about__bio'>
-        <h2>👨‍💻 Code Crafter | 🧘‍♂️ Yoga Yogi | 🌱 Plant Propagating Papi ✨</h2>
-          <p>
-            Embarking on a digital odyssey as a Software Engineering student at UCSB, I'm a tech-savvy wanderer weaving intricate code tapestries. With a heart rooted in mathematics and a mind soaring through creativity, I'm not just a developer – I'm a full-stack virtuoso.
-            When I'm not in the binary realm, you'll find me guiding tranquil yoga sessions or nurturing lush Devil’s Ivy in crystalline containers. From Santa Barbara's shores to the buzzing cannabis industry, I'm a connoisseur of life's diverse flavors. 
-            Join me on this journey of coding, culture, and creating connections. 
-            <br /><br /> Welcome to the realm of possibilities. 🚀🌿🧘‍♂️
-         </p>
-         <a href="#contact" className='btn btn-primary'>Let's Talk!</a>
 
-         </div>
+        <br />
+
+        <div className='about__bio'>
+          <h2>👨‍💻 Code Crafter | 🧘‍♂️ Yoga Yogi | 🌱 Plant Propagating Papi ✨</h2>
+            <p>
+              Embarking on a digital odyssey as a Software Engineering student at UCSB, I'm a tech-savvy wanderer weaving intricate code tapestries. With a heart rooted in mathematics and a mind soaring through creativity, I'm not just a developer – I'm a full-stack virtuoso.
+              When I'm not in the binary realm, you'll find me guiding tranquil yoga sessions or nurturing lush Devil’s Ivy in crystalline containers. From Santa Barbara's shores to the buzzing cannabis industry, I'm a connoisseur of life's diverse flavors. 
+              Join me on this journey of coding, culture, and creating connections. 
+              <br /><br /> Welcome to the realm of possibilities. 🚀🌿🧘‍♂️
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk!</a>
+
+        </div>
+
+      </div>
     </section>
     
   )
